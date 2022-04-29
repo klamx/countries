e1e0f597f2eef64213b4e2422392a2a50f787885
Alejandro Largo <largoalejo@gmail.com>
2022-04-28 21:14:01 -0500

    feat: llamada a la api de REST countries feat: mostrar nombes de paises en pantalla si el array de paises es <= 10 feat: mostrar los datos de un pais en concreto si el array solo contiene un pais

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,73 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
-function App() {
+function App () {
+  const [countries, setcountries] = useState([])
+  const [searchCountry, setSearchCountry] = useState('')
+
+  const handleSearchCountry = (e) => {
+    const country = e.target.value
+    setSearchCountry(country)
+  }
+
+  useEffect(() => {
+    if (searchCountry === '') return
+
+    const getCountriesData = async () => {
+      const response = await axios(
+        `https://restcountries.com/v2/name/${searchCountry}`
+      )
+      return response
+    }
+    getCountriesData().then(({ data }) => {
+      console.log('render')
+      setcountries(data)
+    })
+  }, [searchCountry])
+
   return (
     <div>
-      <div></div>
+      <div>
+        find countries:{' '}
+        <input
+          type='text'
+          placeholder='Search'
+          value={searchCountry}
+          onChange={handleSearchCountry}
+        />
+      </div>
+      {countries.length > 10 && (
+        <div>Too many matches, specify another filter</div>
+      )}
+      {countries.length <= 10 && (
+        <div style={{ marginTop: '1rem' }}>
+          {countries.map((contry) => {
+            return (
+              <div key={contry.name}>
+                <span>{contry.name}</span>
+              </div>
+            )
+          })}
+        </div>
+      )}
+      {countries.length === 1 && (
+        <div style={{ marginTop: '1rem' }}>
+          <h2>{countries[0].name}</h2>
+          <p>capital: {countries[0].capital}</p>
+          <p>population: {countries[0].population}</p>
+          <h3>languajes</h3>
+          <ul>
+            {countries[0].languages.map((lan) => {
+              return <li key={countries[0].name + lan.name}>{lan.name}</li>
+            })}
+          </ul>
+          <img
+            src={countries[0].flag}
+            alt={countries[0].name}
+            style={{ width: '130px' }}
+          />
+        </div>
+      )}
     </div>
   )
 }
