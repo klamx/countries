8bfb7db2e908982d1a52843a485fbb4b4074e5f9
Alejandro Largo <largoalejo@gmail.com>
2022-04-30 12:34:36 -0500

    feat: mostrar temperatura del pais seleccionado

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,30 @@ import Country from './Components/Country'
 function App () {
   const [countries, setcountries] = useState([])
   const [searchCountry, setSearchCountry] = useState('')
+  const [isOneCountry, setIsOneCountry] = useState({ is: false, capital: '' })
+  const [weather, setWeather] = useState({
+    temp: '',
+    img: '',
+    wind: '',
+    dir: '',
+    alt: '',
+    speed: ''
+  })
 
   const handleSearchCountry = (e) => {
     const country = e.target.value
     setSearchCountry(country)
+    if (countries.length === 1) {
+      setIsOneCountry({ is: true, capital: countries[0].capital })
+    } else {
+      setIsOneCountry({ is: false, capital: '' })
+    }
   }
 
   const handleShowCountry = (e) => {
     const country = e.target.value
     setSearchCountry(country)
+    setIsOneCountry({ is: true, capital: countries[0].capital })
   }
 
   useEffect(() => {
@@ -29,11 +44,31 @@ function App () {
     getCountriesData()
       .then(({ data }) => {
         setcountries(data)
+        if (data.length === 1) {
+          setIsOneCountry({ is: true, capital: data[0].capital })
+        }
       })
       .catch((e) =>
         console.log(`%c${e.message}`, 'color: orange; font-size: 14px')
       )
   }, [searchCountry])
+
+  useEffect(() => {
+    const apiKey = import.meta.env.VITE_WEATHER_API_KEY
+    if (!isOneCountry.is) return
+    const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${isOneCountry.capital}&aqi=no`
+    setIsOneCountry({ is: false, capital: '' })
+    axios.get(url).then(({ data }) => {
+      setWeather({
+        temp: data.current.temp_c,
+        wind: data.current.wind_mph,
+        dir: data.current.wind_dir,
+        img: data.current.condition.icon,
+        alt: data.current.condition.text,
+        speed: data.current.wind_mph
+      })
+    })
+  }, [isOneCountry])
 
   return (
     <div>
@@ -55,7 +90,9 @@ function App () {
           handleShowCountry={handleShowCountry}
         />
       )}
-      {countries.length === 1 && <Country countrie={countries[0]} />}
+      {countries.length === 1 && (
+        <Country weather={weather} country={countries[0]} />
+      )}
     </div>
   )
 }
--- src/Components/Country.jsx
+++ src/Components/Country.jsx
@@ -5,22 +5,36 @@ function Country (props) {
         marginTop: '1rem'
       }}
     >
-      <h2>{props.countrie.name}</h2>
-      <p>capital: {props.countrie.capital}</p>
-      <p>population: {props.countrie.population}</p>
+      <h2>{props.country.name}</h2>
+      <p>capital: {props.country.capital}</p>
+      <p>population: {props.country.population}</p>
       <h3>languajes</h3>
       <ul>
-        {props.countrie.languages.map((lan) => {
-          return <li key={props.countrie.name + lan.name}>{lan.name}</li>
+        {props.country.languages.map((lan) => {
+          return <li key={props.country.name + lan.name}>{lan.name}</li>
         })}
       </ul>
       <img
-        src={props.countrie.flag}
-        alt={props.countrie.name}
+        src={props.country.flag}
+        alt={props.country.name}
         style={{
           width: '130px'
         }}
       />
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        <h3>Weather in {props.country.capital}</h3>
+        <div>
+          <b>temperature:</b> {props.weather?.temp} Celcius
+        </div>
+        <img
+          src={props.weather.img}
+          alt={props.alt}
+          style={{ width: '70px' }}
+        />
+        <div>
+          <b>wind:</b> {props.weather.speed} mph direction {props.weather.dir}
+        </div>
+      </div>
     </div>
   )
 }
